93f3a515dc0487b50b652b5cd3da4ef5b2cc180d
Alon Fine <mecheniy5462@gmail.com>
2023-11-25 23:50:22 +0200

    created close-open functionality to post window by using ref(there we check if current ref not contains event target from all entire document)

--- src/shared/CardsList/Card/TextContent/Title/Title.tsx
+++ src/shared/CardsList/Card/TextContent/Title/Title.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import styles from '../../card.css';
 import {Post} from "../../../../Post";
 
@@ -8,18 +8,22 @@ interface ITitleProps {
 
 export function Title({title}: ITitleProps) {
     const [isModalOpen, setIsModalOpen] = useState(false)
-    const handelClick = () => {
-        console.log('clicked')
-        setIsModalOpen(!isModalOpen)
+    const onOpen = () => {
+        console.log('modal is Open')
+        setIsModalOpen(true)
     }
-
+    const onClose = () => {
+        console.log('modal is Close')
+        setIsModalOpen(false)
+    }
     return (
-        <h2 className={styles.title} onClick={handelClick}>
+        <h2 className={styles.title} onClick={onOpen}>
             <button className={styles.postLink}>
-                {title ? title
+                {title
+                    ? title
                     : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, voluptas? Iste.'}
             </button>
-            {isModalOpen && <Post/>}
+            {isModalOpen && <Post onClose={onClose}/>}
         </h2>
     );
 }
